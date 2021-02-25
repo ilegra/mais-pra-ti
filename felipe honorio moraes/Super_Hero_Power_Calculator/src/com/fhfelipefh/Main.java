6522cc36c4c8c59dea0b457f52bd8964cf5bf566
FELIPE HONORIO MORAES <felipehonorio55@gmail.com>
2021-02-25 13:00:47 -0300

    update
    deletar cpp

--- felipe honorio moraes/Super_Hero_Power_Calculator/src/com/fhfelipefh/Main.java
+++ felipe honorio moraes/Super_Hero_Power_Calculator/src/com/fhfelipefh/Main.java
@@ -8,33 +8,33 @@ public class Main {
         double forca=1;
         double aux;
         double aux2;
-        double dist_vis=0;
+        double distVis=0;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Insira a quantidade de dias que ele está na terra: ");
         int dias = scanner.nextInt();
         if(dias<=365)
         {
             System.out.println("Não adquiriu força devido a adaptação!");
-            System.out.println("Força: "+forca+" Humano(s) e a distância que sua visão de calor atinge (cm) "+dist_vis);
+            System.out.println("Força: "+forca+" Humano(s) e a distância que sua visão de calor atinge (cm) "+distVis);
         }
         else{
             dias = dias - 365;
             for(int i = 0; i<dias; ++i){
                 aux = forca*0.03;
-                forca=forca+aux;
+                forca += forca*0.03;
             }
             if(forca>=10)
             {
-                dist_vis = 10;
+                distVis = 10;
                 for(int j = 0; j<dias; j++)
                 {
-                    aux2 = dist_vis*0.02;
-                    dist_vis=dist_vis+aux2;
+                    aux2 = distVis*0.02;
+                    distVis=distVis+aux2;
 
                 }
             }
         }
         System.out.printf("Força: %.4f Humano(s) %n",forca);
-        System.out.printf("Distância que sua visão de calor atinge: %.4f cm",dist_vis);
+        System.out.printf("Distância que sua visão de calor atinge: %.4f cm",distVis);
 }
 }
