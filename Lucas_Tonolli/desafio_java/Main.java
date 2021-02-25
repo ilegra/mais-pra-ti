ef3ab717785fa2b1722cea7bb64fefb0d90b9dda
LucasTonolli <tonollilucas@yahoo.com>
2021-02-25 09:37:58 -0300

    Desafio Java Refeito

--- Lucas_Tonolli/desafio_java/Main.java
+++ Lucas_Tonolli/desafio_java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Superman superman = new Superman(443, 1);
+        superman.SuperPowerCalculator();
     }
 }
--- Lucas_Tonolli/desafio_java/Superman.java
+++ Lucas_Tonolli/desafio_java/Superman.java
@@ -1,49 +1,67 @@
 public class Superman {
     //propriedades da classe
-    private double forca;
-    private double distanciaVisaoCalor;
+    public double forca;
+    public double distanciaVisaoCalor;
     public int dias;
     public float forcaHumana;
+    public boolean desbloqueioVisao;
 
-//Recebe o quantos dias e força base do humano comum(construtor)
-    public Superman(int dias,int forca_humano){
-            this.forca = 1.0;
-            this.distanciaVisaoCalor = 0.0;
-        //variavél que faz só haver 1 acréscimo de +10 na distância da visão
-        boolean desbloqueioVisao = false;
-            this.dias = dias;
-            this.forcaHumana = forca_humano;
-
-        if(dias<=365){
-            System.out.println("O Superman não teve tempo suficiente para se adaptar!");
-        }else{
-            for(int i =dias-365; i>0;i--){
-                forca+=(forca*0.03);
-                    if(forca>=(forcaHumana*10)&& !desbloqueioVisao){
-                        distanciaVisaoCalor = 10.0;
-                        desbloqueioVisao = true;
-                    }else if(forca>=(forcaHumana*10)&& desbloqueioVisao){
-                        distanciaVisaoCalor+=(distanciaVisaoCalor*0.02);
-                    }
-                }
-            }
-        System.out.println("A força do Superman é  " + forca);
-        System.out.println("A a distância máxima da visão de calor do Superman é  " + distanciaVisaoCalor + "cm");
+    //Construtor
+    public Superman(int dias, int forcaHumano) {
+        this.forca = 1.0;
+        this.distanciaVisaoCalor = 0.0;
+        //variável que faz só haver 1 acréscimo de +10 na distância da visão
+        this.desbloqueioVisao = false;
+        this.dias = dias;
+        this.forcaHumana = forcaHumano;
     }
- //modificadores
-    public double getforca(){
+
+    public void SuperPowerCalculator() {
+        if (dias <= 365) {
+            System.out.println("O Superman não teve tempo suficiente para se adaptar!");
+        } else {
+            distanciaVisaoCalor = VisaoDeCalor();
+            forca = ForcaSuperman();
+        }
+        System.out.println("A força do Superman é "+forca);
+        System.out.println("A a distância máxima da visão de calor do Superman é "+ distanciaVisaoCalor +" cm");
+}
+
+    public double ForcaSuperman() {
+        for (int i = dias - 365; i > 0; i--) {
+            forca += (forca * 0.03);
+        }
         return forca;
     }
 
-    public double getdistancia_visao_calor(){
+    public double VisaoDeCalor() {
+        for (int i = dias - 365; i > 0; i--) {
+            forca += (forca * 0.03);
+            if(forca>=(forcaHumana*10)&& !desbloqueioVisao){
+                distanciaVisaoCalor = 10.0;
+                desbloqueioVisao = true;
+            }else if(forca>=(forcaHumana*10)&& desbloqueioVisao){
+                distanciaVisaoCalor+=(distanciaVisaoCalor*0.02);
+            }
+        }
+        forca = 1.0;
         return distanciaVisaoCalor;
     }
 
-    public void setforca(double forca){
+ //modificadores
+    public double getForca(){
+        return forca;
+    }
+
+    public double getDistanciaVisaoCalor(){
+        return distanciaVisaoCalor;
+    }
+
+    public void setForca(double forca){
         this.forca = forca;
     }
 
-    public void setdistancia_visao_calor(double distancia_visao_calor){
-        this.distanciaVisaoCalor = distancia_visao_calor;
+    public void setDistanciaVisaoCalor(double distanciaVisaoCalor){
+        this.distanciaVisaoCalor = distanciaVisaoCalor;
     }
 }
