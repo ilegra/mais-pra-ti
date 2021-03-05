7c753c58a5f3294f77a76e4330c946b54ecd6d9b
LucasTonolli <78648345+LucasTonolli@users.noreply.github.com>
2021-03-05 10:50:42 -0300

    Add files via upload

--- Lucas_Tonolli/Desafio_Orien_Obj/src/Candidato.java
+++ Lucas_Tonolli/Desafio_Orien_Obj/src/Candidato.java
@@ -1,9 +1,19 @@
 public abstract class Candidato {
     private String nome;
+<<<<<<< Updated upstream
 
 
     public Candidato(String nome){
         this.nome = nome;
+=======
+    public int codCandidato;
+    public int numVotos;
+
+    public Candidato(String nome,int codCandidato){
+        this.nome = nome;
+        this.codCandidato = codCandidato;
+        this.numVotos = 0;
+>>>>>>> Stashed changes
     }
 
     public abstract String Info();
@@ -16,4 +26,24 @@ public abstract class Candidato {
         this.nome = nome;
     }
 
+<<<<<<< Updated upstream
+=======
+    public int getCodCandidato() {
+        return codCandidato;
+    }
+
+    public void setCodCandidato(int codCandidato) {
+        this.codCandidato = codCandidato;
+    }
+    private void setNumVotos(int numVotos){
+        this.numVotos=numVotos;
+    }
+    public void recebendoVotos(){
+        this.numVotos ++;
+    }
+    public int getNumVotos(){
+        return numVotos;
+    }
+
+>>>>>>> Stashed changes
 }
