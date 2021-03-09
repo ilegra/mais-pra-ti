e30db891025e38a6036c3fb37be854b5e04d9089
LucasTonolli <78648345+LucasTonolli@users.noreply.github.com>
2021-03-09 16:58:16 -0300

    Update DeptFederal.java

--- Lucas_Tonolli/Desafio_Orien_Obj/src/DeptFederal.java
+++ Lucas_Tonolli/Desafio_Orien_Obj/src/DeptFederal.java
@@ -1,45 +1,11 @@
 public class DeptFederal extends Candidato{
-<<<<<<< Updated upstream
-    private int CodCandidato;
-    private int NumVotos;
-
-    public DeptFederal(String nome, int CodCandidato){
-        super(nome);
-        this.CodCandidato = CodCandidato;
-        this.NumVotos = 0;
-=======
 
     public DeptFederal(String nome, int codCandidato){
         super(nome,codCandidato);
->>>>>>> Stashed changes
     }
 
     @Override
     public String Info(){
         return "Candidato a Deputado Federal";
     }
-<<<<<<< Updated upstream
-
-    public int getCodCandidato() {
-        return CodCandidato;
-    }
-
-    public void setCodCandidato(int codCandidato) {
-        CodCandidato = codCandidato;
-    }
-
-    public int getNumVotos() {
-        return NumVotos;
-    }
-
-    public void recebendoVotos(){
-        this.NumVotos ++;
-    }
-
-    private void setNumVotos(int NumVotos) {
-        NumVotos = NumVotos;
-    }
-
-=======
->>>>>>> Stashed changes
 }
