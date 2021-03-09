9fb103871a8990c262709dd089f3ec203937b4e7
LucasTonolli <78648345+LucasTonolli@users.noreply.github.com>
2021-03-09 16:59:29 -0300

    Update Main.java

--- Lucas_Tonolli/Desafio_Orien_Obj/src/Main.java
+++ Lucas_Tonolli/Desafio_Orien_Obj/src/Main.java
@@ -5,22 +5,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //Cadastro de participantes
-<<<<<<< Updated upstream
-       DeptFederal candF1 = new DeptFederal("Roberto",1259);
-       DeptFederal candF2 = new DeptFederal("Luiz",1879);
-       DeptEstadual candE1 = new DeptEstadual("João",195);
-       DeptEstadual candE2 = new DeptEstadual("Wallace",15);
-
-       //Criação da lista de partipantes
-       List<DeptFederal> candDeptFed = new ArrayList<>();
-       List<DeptEstadual> candDeptEst = new ArrayList<>();
-
-       //Inserindo participantes nas listas
-       candDeptEst.add(candE1);
-       candDeptEst.add(candE2);
-       candDeptFed.add(candF1);
-       candDeptFed.add(candF2);
-=======
        Candidato candF1 = new DeptFederal("Roberto",1259);
        Candidato candF2 = new DeptFederal("Luiz",1879);
        Candidato candE1 = new DeptEstadual("João",195);
@@ -35,7 +19,7 @@ public class Main {
        candidatosEleicao.add(candF2);
        candidatosEleicao.add(candE1);
        candidatosEleicao.add(candE2);
->>>>>>> Stashed changes
+
 
        //Variáveis para definir ganhador
         int eleitoE = 0;
@@ -48,18 +32,7 @@ public class Main {
        Scanner leituraVoto = new Scanner(System.in);
        int voto;
        boolean verificacaoCand = false;
-<<<<<<< Updated upstream
-       int j = 0;
 
-        System.out.println("Eleição acaba no momento em que for digitado '0'\n");
-        for(int i = 0; i < candDeptEst.size(); i++ ) {
-            System.out.println("Candidato Estadual: " + candDeptEst.get(i).getNome() +
-                    " Código: " + candDeptEst.get(i).getCodCandidato() +"\n");
-        }
-        for(int i = 0; i < candDeptEst.size(); i++ ) {
-            System.out.println("Candidato Federal: " + candDeptFed.get(i).getNome() +
-                    " Código: " + candDeptFed.get(i).getCodCandidato() +"\n");
-=======
        //Variável para encontrar o ganhador
        int j = 0;
 
@@ -72,28 +45,18 @@ public class Main {
              System.out.println("Candidato à Deputado Estadual: " + candidatosEleicao.get(i).getNome() +
                      "\n" + "Código: " + candidatosEleicao.get(i).getCodCandidato() + "\n");
          }
->>>>>>> Stashed changes
+
         }
        do{
            do{
                System.out.println("Digite o código do candidato: ");
                voto = leituraVoto.nextInt();
 
-<<<<<<< Updated upstream
-                for(int i = 0; i < candDeptEst.size(); i++ ) {
-                    if (voto == candDeptEst.get(i).getCodCandidato()) {
-                        System.out.println(candDeptEst.get(i).Info());
-                        candDeptEst.get(i).recebendoVotos();
-                        verificacaoCand = true;
-                    }else if (voto == candDeptFed.get(i).getCodCandidato()) {
-                        System.out.println(candDeptFed.get(i).Info());
-                        candDeptFed.get(i).recebendoVotos();
-=======
                 for(int i = 0; i < candidatosEleicao.size(); i++ ) {
                     if (voto == candidatosEleicao.get(i).getCodCandidato()) {
                         System.out.println(candidatosEleicao.get(i).Info());
                         candidatosEleicao.get(i).recebendoVotos();
->>>>>>> Stashed changes
+
                         verificacaoCand = true;
                     }
                 }
@@ -101,50 +64,14 @@ public class Main {
                    System.out.println("Código não cadastrado\n");
                }
             }while(!verificacaoCand && voto!=0);
-<<<<<<< Updated upstream
-           verificacaoCand = false;
-            if(voto!=0)
-=======
 
            verificacaoCand = false;
             if(voto!=0 && voto > 0)
->>>>>>> Stashed changes
                 TotalVotos++;
        }while(voto!=0);
 
         System.out.println("Total de votos " + TotalVotos+ "\n");
 
-<<<<<<< Updated upstream
-        for(int i = 0; i < candDeptEst.size(); i++ ){
-            if(candDeptEst.get(i).getNumVotos()>maiorE){
-              eleitoE = candDeptEst.get(i).getCodCandidato();
-              maiorE = candDeptEst.get(i).getNumVotos();
-            }
-            System.out.println("Candidato Estadual: " + candDeptEst.get(i).getNome() +
-                    " recebeu " +candDeptEst.get(i).getNumVotos() +" votos\n");
-        }
-        for(int i = 0; i < candDeptFed.size(); i++ ){
-            if(candDeptFed.get(i).getNumVotos()>maiorF){
-                eleitoF = candDeptFed.get(i).getCodCandidato();
-                maiorF = candDeptFed.get(i).getNumVotos();
-            }
-            System.out.println("Candidato Federal: " + candDeptFed.get(i).getNome() +
-                    " recebeu " +candDeptFed.get(i).getNumVotos() +" votos\n");
-        }
-        while(eleitoE!=candDeptEst.get(j).getCodCandidato()){
-            j++;
-        }
-        System.out.println("O deputado estadual eleito foi : " + candDeptEst.get(j).getNome());
-        j = 0;
-        while(eleitoF!=candDeptFed.get(j).getCodCandidato()){
-            j++;
-        }
-        System.out.println("O deputado Federal eleito foi : " + candDeptFed.get(j).getNome());
-
-
-    }
-}
-=======
         for(int i = 0; i < candidatosEleicao.size(); i++ ) {
             if (candidatosEleicao.get(i) instanceof DeptEstadual) {
                 if (candidatosEleicao.get(i).getNumVotos() > maiorE) {
@@ -174,4 +101,4 @@ public class Main {
     }
 }
 
->>>>>>> Stashed changes
+
