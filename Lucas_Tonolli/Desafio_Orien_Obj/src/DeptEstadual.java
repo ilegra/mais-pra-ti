e8687680f3992e8adb7fd33beed9daeb1c7620fb
LucasTonolli <78648345+LucasTonolli@users.noreply.github.com>
2021-03-10 09:31:37 -0300

    Update DeptEstadual.java

--- Lucas_Tonolli/Desafio_Orien_Obj/src/DeptEstadual.java
+++ Lucas_Tonolli/Desafio_Orien_Obj/src/DeptEstadual.java
@@ -1,25 +1,6 @@
 
 public class DeptEstadual extends Candidato{
 
-
-
-    public DeptEstadual(String nome, int codCandidato){
-        super(nome,codCandidato);
-
-    }
-
-    @Override
-    public String Info(){
-        return "Candidato a Deputado Estadual";
-    }
-
-
-
-
-
-
-}
-
 public class DeptEstadual extends Candidato{
 
     public DeptEstadual(String nome, int codCandidato){
